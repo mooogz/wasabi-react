dd8db4ab4930f75cf75ce269076308eca33f44ad
Megan McCaw <meganm.mccaw@gmail.com>
2025-05-28 22:04:54 -0400

    updated links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,8 +8,8 @@ const Navbar = ({toggle}) => {
         <Link to="/"><img src={logo} alt="Wasabi Logo" className="mx-auto h-25" /></Link>
 
         <nav className="mt-4 flex flex-col items-center sm:flex-row justify-center space-y-2 sm:space-y-0 sm:space-x-8 text-gray-900">
-            <a href="/" className="px-2 py-1 hover:text-[#4c7a42] transition-colors duration-300">Home</a>
-            <a href="/menu" className="px-2 py-1 hover:text-[#4c7a42] transition-colors duration-300">Menu</a>
+            <Link to="/" className="px-2 py-1 hover:text-[#4c7a42] transition-colors duration-300">Home</Link>
+            <Link to="/menu" className="px-2 py-1 hover:text-[#4c7a42] transition-colors duration-300">Menu</Link>
             <a href="tel:9102293498" className="px-2 py-1 bg-green-700 text-white rounded-lg hover:bg-green-600 transition-colors duration-300">
                 910-229-3498</a>
         </nav>

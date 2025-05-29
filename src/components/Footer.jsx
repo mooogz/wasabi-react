cbc3e0b78e54632c95064a6e5050831a09811a62
Megan McCaw <meganm.mccaw@gmail.com>
2025-05-28 22:18:02 -0400

    changed link in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <section className="text-center py-6 mt-10">
       <footer>
         <p className="text-sm text-white">© Copyright 2025 Wasabi Japanese Restaurant</p>
-        <p className="text-xs mt-2 text-white">Website built with React & 💚 by <a href="https://github.com/mooogz" className="underline">Megan McCaw</a></p>
+        <p className="text-xs mt-2 text-white">Website built with React by <a href="https://github.com/mooogz" className="underline">Megan May</a></p>
       </footer>
     </section>
   )
